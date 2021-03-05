4579529dce95e92721a295b8f19de5c821fe498e
vanyusuf1 <yusufvan71@gmail.com>
2021-03-05 22:13:43 +0530

    starter pack confgurations

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const User = require("./models/user");
+
+const port = process.env.PORT || 3000;
+app.use(express.json());
+require("dotenv/config");
+
+
+app.get("/", (req, res) => {
+  res.send("hello Grofers!!");
+});
+
+// app.post("/students", (req, res) => {
+//   res.send("hello all");
+// });
+
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  (req, res) => {
+    console.log("Connected to the database");
+  }
+);
+
+
+const userRoute = require("./routes/users");
+app.use("/users", userRoute);
+
+const raffleEvent = require("./routes/raffleEvent");
+app.use("/raffleEvent", raffleEvent);
+
+app.listen(port, () => {
+  console.log(`connections is setup ${port}`);
+});
+
+
