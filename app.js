3c938bdadc2babd2459e53944835ff5fc5a128aa
vanyusuf1 <yusufvan71@gmail.com>
2021-03-06 10:29:49 +0530

    changes in routes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const User = require("./models/user");
 
 const port = process.env.PORT || 3000;
 app.use(express.json());
